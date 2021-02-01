d52966f1c8e9f626dbcb419db7d12ab68c77fe22
jonghyun <jonghyeon2046@gmail.com>
2021-02-01 17:27:38 +0900

    signin update

---
+++ frontend/src/api.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { apiUrl } from './config.js';
+
+export const getProduct = async id => {
+    try {
+        const response = await axios({
+            url: `${apiUrl}/api/products/${id}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.message };
+    }
+};
+
+export const signin = async ({ email, password }) => {
+    try {
+        const response = await axios({
+            url: `${apiUrl}/api/users/signin`,
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                email,
+                password,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.message };
+    }
+};
---
+++ frontend/src/index.js
@@ -0,0 +1,33 @@
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+import Error404Screen from './screens/Error404Screen.js';
+import CartScreen from './screens/CartScreen';
+import { parseRequestUrl } from './utils.js';
+import SigninScreen from './screens/SigninScreen.js';
+import Header from './components/Header.js';
+
+const routes = {
+    '/': HomeScreen,
+    '/product/:id': ProductScreen,
+    '/cart/:id': CartScreen,
+    '/cart': CartScreen,
+    '/signin': SigninScreen,
+};
+
+const router = async () => {
+    const request = parseRequestUrl();
+    const parseUrl =
+        (request.resource ? `/${request.resource}` : '/') +
+        (request.id ? '/:id' : '') +
+        (request.verb ? `/${request.verb}` : '');
+    const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+    const header = document.getElementById('header-container');
+    header.innerHTML = await Header.render();
+    await Header.after_render();
+    const main = document.getElementById('main-container');
+    main.innerHTML = await screen.render();
+    await screen.after_render();
+};
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
