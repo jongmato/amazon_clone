ea729819ddc6cf4d109f54a3effd1be21b55cca7
jonghyun <jonghyeon2046@gmail.com>
2021-02-17 20:42:53 +0900

    [UPDATE]data id 수정

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 export default {
     products: [
         {
-            _id: "1",
+            _id: "111111111111111111111111",
             name: "Hiskywin Full Zip Running Shirts Thermal Workout",
             category: "Shirts",
             image: "/images/product-1.jpg",
@@ -12,7 +12,7 @@ export default {
             countInStock: 6,
         },
         {
-            _id: "2",
+            _id: "222222",
             name: "Nike Full-Zip Hoodie Mens Workout Sweatshirt",
             category: "Shirts",
             image: "/images/product-2.jpg",
