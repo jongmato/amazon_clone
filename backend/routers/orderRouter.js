b58c31cd08ef6fea2214e4cb7d35b325ef0d6a1f
jonghyun <jonghyeon2046@gmail.com>
2021-02-18 10:37:56 +0900

    [UPDATE]

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -40,4 +40,24 @@ orderRouter.post(
     }),
 );
 
+orderRouter.put(
+    '/:id/pay',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.payment.paymentResult = {
+                payerID: req.body.payerID,
+                paymentID: req.body.paymentID,
+                orderID: req.body.orderID,
+            };
+            const updateOrder = await order.save();
+            res.send({ message: 'Order Paid', order: updateOrder });
+        } else {
+            res.status(404).send({ message: 'Order Not Found.' });
+        }
+    }),
+);
 export default orderRouter;
