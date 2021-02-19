66d6649b544cadc265e72298969911c6e290034c
jonghyun <jonghyeon2046@gmail.com>
2021-02-19 15:56:57 +0900

    [UPDATE]admin check

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { getUserInfo } from '../localStorage';
 
 const Header = {
     render: () => {
-        const { name } = getUserInfo();
+        const { name, isAdmin } = getUserInfo();
         return `
         <div class="brand">
             <a href="/#/">jsamazona</a>
@@ -14,6 +14,7 @@ const Header = {
                     : `<a href="/#/signin">Sign-in</a>`
             }
             <a href="/#/cart">Cart</a>
+            ${isAdmin ? `<a href="/#/dashboard">Dashboard</a>` : ''}
         </div>
         `;
     },
