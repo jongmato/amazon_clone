5109b4e88859f9f67d14113cec58f91784cea6ea
jonghyun <jonghyeon2046@gmail.com>
2021-05-29 21:59:10 +0900

    [UPDATE]deliverOrder api add

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -335,6 +335,28 @@ export const payOrder = async (orderId, paymentResult) => {
     }
 };
 
+export const deliverOrder = async orderId => {
+    try {
+        const { token } = getUserInfo();
+        const response = await axios({
+            url: `${apiUrl}/api/orders/${orderId}/deliver`,
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        return {
+            error: err.response ? err.response.data.message : err.message,
+        };
+    }
+};
+
 export const getSummary = async () => {
     try {
         const { token } = getUserInfo();
