c6e4d99f6ae9ab80a72c039d86c8bc166e4f3d29
jonghyun <jonghyeon2046@gmail.com>
2021-05-29 10:03:29 +0900

    [UPDATE]관리자 체크 함수 추가

--- backend/utils.js
+++ backend/utils.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import config from './config';
 
-export const generateToken = user => {
-    return jwt.sign(
+export const generateToken = user =>
+    jwt.sign(
         {
             _id: user._id,
             name: user.name,
@@ -11,7 +11,6 @@ export const generateToken = user => {
         },
         config.JWT_SECRET,
     );
-};
 
 export const isAuth = (req, res, next) => {
     const bearerToken = req.headers.authorization;
@@ -27,5 +26,13 @@ export const isAuth = (req, res, next) => {
                 next();
             }
         });
+    }
+};
+
+export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({ message: 'Token is not valid for admin user' });
     }
 };
