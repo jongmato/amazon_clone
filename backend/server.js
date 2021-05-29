9a432a1ac86a774e7db050a028a8517317b62e61
jonghyun <jonghyeon2046@gmail.com>
2021-05-29 10:04:16 +0900

    [UPDATE]product router add

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import data from './data.js';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import data from './data';
 import config from './config';
 import userRouter from './routers/userRouter';
-import bodyParser from 'body-parser';
-import orderRouter from './routers/orderRouter.js';
+import orderRouter from './routers/orderRouter';
+import productRouter from './routers/productRouter';
 
 mongoose
     .connect(config.MONGODB_URL, {
@@ -23,6 +24,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/paypal/clientId', (req, res) => {
     res.send({ clientId: config.PAYPAL_CLIENT_ID });
